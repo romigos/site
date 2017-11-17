6237e6bb4aa2e1e139a59b2a1b4d8beb2eb859dc
Roman Bolistovskyi <bolist@mail.ru>
2017-11-17 07:51:34 +0200

    initial commit

---
+++ lesson3/variables.rb
@@ -0,0 +1,19 @@
+hello = "привет пацани!"
+hello2 = "пойдем на дискотеку"
+hello3 = "пойдем потанцуем" 
+hello4 = "пойдем покурим"
+ puts hello
+ devchenki = true
+  if (devchenki == true)
+ 	puts hello2
+ 	puts hello3
+ else 
+
+    if (devchenki== false)
+ 
+ puts hello4
+    	
+    end
+
+
+ end
