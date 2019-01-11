268df44bd1f40e3226ae3d2f0d62ec5a6da11fd3
romigos <bolist@mail.ru>
2019-01-11 13:52:55 +0200

    step 9

---
+++ lesson3/digital.rb
@@ -0,0 +1,13 @@
+number1 = 4
+number2 = 36
+puts number1, number2
+if number1 > number2
+  puts 'Наибольшее число:' + number1.to_s
+else
+  puts 'Наибольшее число:' + number2.to_s
+end
+if number1 == number2
+  puts 'Числа рівні'
+else
+  puts 'Числа не рівні'
+end
