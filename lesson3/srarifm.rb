4831fd03910ad87bf143094d42a74b8c97bcbd06
romigos <bolist@mail.ru>
2019-01-11 15:05:35 +0200

    step 10

---
+++ lesson3/srarifm.rb
@@ -0,0 +1,6 @@
+number1 = 2308
+number2 = 12
+  puts 'Первое число:' + number1.to_s
+  puts 'Второе число:' + number2.to_s
+average = (number1 + number2) / 2
+  puts 'Среднее арифметическое:' + average.to_s
